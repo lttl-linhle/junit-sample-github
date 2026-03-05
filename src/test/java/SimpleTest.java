562807766a6e53839ad99fd5f20e3f72224a7b03
Truc Linh <letruongtruclinh2004@gmail.com>
2026-03-05 20:36:03 +0700

    init

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SimpleTest {
+    @Test
+    void testAgileTestIntegration() {
+        System.out.println("AgileTest integration practice...");
+        assertTrue(true);
+    }
+}
